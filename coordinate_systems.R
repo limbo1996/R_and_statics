143ea496d8861bef2dcdf93069e15ef18d673be1
limbo1996 <wangxuan1@shangiatech.edu.cn>
2020-01-11 21:59:59 +0800

    坐标系

---
+++ coordinate_systems.R
@@ -0,0 +1,7 @@
+# coord_flip()可以转换x,y轴
+ggplot(data = mpg, mapping = aes(x = class, y = hwy)) +
+  geom_boxplot()
+
+ggplot(data = mpg, mapping = aes(x = class, y = hwy)) +
+  geom_boxplot() + 
+  coord_flip()
