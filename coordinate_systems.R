b544ca4fbcaed3b42e9454322b381b36015d85d0
limbo1996 <wangxuan1@shanghaitech.edu.cn>
2020-05-28 22:46:11 +0800

    change files

--- coordinate_systems.R
+++
@@ -1,7 +0,0 @@
-# coord_flip()可以转换x,y轴
-ggplot(data = mpg, mapping = aes(x = class, y = hwy)) +
-  geom_boxplot()
-
-ggplot(data = mpg, mapping = aes(x = class, y = hwy)) +
-  geom_boxplot() + 
-  coord_flip()
